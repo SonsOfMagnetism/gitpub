c4d7de5f677cb14478a34cadb7450da4a882c417
Rene Santiago <rsj510@gmail.com>
2022-11-10 14:32:36 -0500

    added drinks

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const drinks = require('./models/drinks')
 const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
     res.send(`Welcome to the Gitpub App!`)
+})
+
+app.get('/drinks/', (req, res) => {
+    res.send(drinks)
 })
 
 app.listen(PORT, () => {
