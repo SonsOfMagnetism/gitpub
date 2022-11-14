633f952c41800cecad1967bb1a3fda81f5462def
Rene Santiago <rsj510@gmail.com>
2022-11-14 15:41:06 -0500

    added drinks to index

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const drinks = require('./models/drinks')
 const app = express()
 const PORT = 3000
 
+app.get('/drinks/', (req, res) => {
+    res.render("drinks_index.ejs", {
+        allDrinks: drinks,
+    })
+})
+
 app.get('/', (req, res) => {
     res.send(`Welcome to the Gitpub App!`)
 })
 
-app.get('/drinks/', (req, res) => {
-    res.render("drinks_index.ejs")
-})
+
 
 app.listen(PORT, () => {
     console.log(`Currently listening to Port ${PORT}`)
