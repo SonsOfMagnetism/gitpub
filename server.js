cc3df5a3e6e45a35e83e9d7fa70d81be4b8c6850
Rene Santiago <rsj510@gmail.com>
2022-11-14 15:44:16 -0500

    setup show route

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const drinks = require('./models/drinks')
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+    res.send(`Welcome to the Gitpub App!`)
+})
+
+// Index Route
 app.get('/drinks/', (req, res) => {
     res.render("drinks_index.ejs", {
         allDrinks: drinks,
     })
 })
 
-app.get('/', (req, res) => {
-    res.send(`Welcome to the Gitpub App!`)
+// Show Route
+app.get('/drinks/:id', (req, res) => {
+    res.send(req.params.id)
 })
-
-
 
 app.listen(PORT, () => {
     console.log(`Currently listening to Port ${PORT}`)
