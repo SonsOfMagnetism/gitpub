4d54e41054fb0db7bcef70d098c97f552ed5c089
Rene Santiago <rsj510@gmail.com>
2022-11-14 21:24:47 -0500

    added food

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const drinks = require('./models/drinks')
+const food = require('./models/food')
 const app = express()
 const PORT = 3000
 
@@ -7,17 +8,32 @@ app.get('/', (req, res) => {
     res.send(`Welcome to the Gitpub App!`)
 })
 
-// Index Route
+// Drink Index Route
 app.get('/drinks/', (req, res) => {
     res.render("drinks_index.ejs", {
         allDrinks: drinks,
     })
 })
 
-// Show Route
+// Drink Show Route
 app.get('/drinks/:id', (req, res) => {
     res.render("drinks_show.ejs", {
         drink: drinks[req.params.id],
+        id: req.params.id,
+    })
+})
+
+// Food Index Route
+app.get('/food/', (req, res) => {
+    res.render("food_index.ejs", {
+        allFood: food,
+    })
+})
+
+// Food Show Route
+app.get('/food/:id', (req, res) => {
+    res.render("food_show.ejs", {
+        food: food[req.params.id],
         id: req.params.id,
     })
 })
